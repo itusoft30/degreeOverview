a6e0d09735f5213a1f66055c6b3eb0fa866fff00
İskender Akyüz <akyuziskender@hotmail.com>
2019-12-14 21:09:55 +0300

    Basic SQL functions for COURSE and REGUSER tables were written. A mistake in intDb.py was fixed. (Name attribute was added to the course table)

---
+++ project/models/course.py
@@ -0,0 +1,24 @@
+import psycopg2 as dbapi2
+
+class Course:
+    @staticmethod
+    def AddCourse(crn, name, courseCode, instructorID, credit, departmentID):
+        with dbapi2.connect(url) as connection:
+            statement = """INSERT INTO COURSE (Crn, Name, CourseCode, InstructorId, Credit, DepartmentID) VALUES (%s, %s, %s, %d, %d, %d);"""
+            cursor.execute(statement, (crn, name, courseCode, instructorId, credit, departmentID))
+            cursor.close()
+
+    @staticmethod
+    def ModifyCourse(courseID, crn, name, courseCode, instructorID, credit, departmentID):
+        with dbapi2.connect(url) as connection:
+            statement = """UPDATE COURSE SET (Crn = %s, Name = %s, CourseCode = %s, InstructorID = %d, Credit = %d, DepartmentID = %d) WHERE (CourseID = %d);"""
+            cursor.execute(statement, (crn, name, courseCode, instructorId, credit, departmentID, courseID))
+            cursor.close()
+
+    @staticmethod
+    def DeleteCourse(courseID):
+        with dbapi2.connect(url) as connection:
+            cursor = connection.cursor()
+            statement = """DELETE FROM COURSE WHERE (CourseID = %d);"""
+            cursor.execute(statement, (id,))
+            cursor.close()
