e55279f7681776b9844e487e6433d7050b87dc3b
Memin <topuzm15@gmail.com>
2019-12-15 17:30:29 +0300

    Update for relationships.

--- project/models/models.py
+++ project/models/models.py
@@ -4,7 +4,9 @@ from project import app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eihjewuminuans:a7c73a627a7488f86e19d477de45967dc9abd2e85591958438627d0cf4e275a0@ec2-54-247-96-169.eu-west-1.compute.amazonaws.com:5432/d2jv9l5jpv6eep'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+#dchvhdd
+x = 10
+x = x+2
 class Department(db.Model):
     __tablename__ = 'department'
     department_id = db.Column(db.Integer, primary_key=True)
