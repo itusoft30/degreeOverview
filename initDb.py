726beec41d829893f1dc5530603e22185dcad764
Memin <topuzm15@gmail.com>
2019-12-15 17:46:43 +0300

    Renaming

--- initDb.py
+++ initDb.py
@@ -7,62 +7,61 @@ import psycopg2 as dbapi2
 INIT_STATEMENTS = [
     """
     CREATE TABLE DEPARTMENT ( 
-                    DepartmentID SERIAL PRIMARY KEY, 
-                    DepartmentName VARCHAR(50), 
-                    FacultyName VARCHAR(50)
+                    Department_ID SERIAL PRIMARY KEY, 
+                    Department_Name VARCHAR(50), 
+                    Faculty_Name VARCHAR(50)
                     );
     CREATE TABLE REGUSER ( 
-                    UserID SERIAL PRIMARY KEY, 
+                    User_ID SERIAL PRIMARY KEY, 
                     Name VARCHAR(30), 
                     Surname VARCHAR(30),
                     Email VARCHAR(50) UNIQUE NOT NULL,
                     Password VARCHAR(100) NOT NULL,
-                    DepartmentID INTEGER REFERENCES DEPARTMENT (DepartmentID),
-                    IsAdmin INTEGER DEFAULT 0
+                    Department_ID INTEGER REFERENCES DEPARTMENT (Department_ID),
+                    User_type INTEGER DEFAULT 0
                     );
     CREATE TABLE INSTRUCTOR ( 
-                    InstructorID INTEGER REFERENCES REGUSER (UserID) UNIQUE, 
+                    Instructor_ID INTEGER REFERENCES REGUSER (User_ID) UNIQUE, 
                     Title VARCHAR(50)
                     );
     CREATE TABLE STUDENT ( 
-                    StudentID INTEGER REFERENCES REGUSER (UserID) UNIQUE,
-                    IdNumber VARCHAR(9) 
+                    Student_ID INTEGER REFERENCES REGUSER (User_ID) UNIQUE,
+                    Id_Number VARCHAR(9) 
                     );
     CREATE TABLE COURSE ( 
-                    CourseID SERIAL PRIMARY KEY, 
+                    Course_ID SERIAL PRIMARY KEY, 
                     Crn VARCHAR(5),
-                    Name VARCHAR(50),
-                    CourseCode VARCHAR(10),
-                    InstructorID INTEGER REFERENCES INSTRUCTOR (InstructorID),
+                    Course_Code VARCHAR(10),
+                    Instructor_ID INTEGER REFERENCES INSTRUCTOR (Instructor_ID),
                     Credit INTEGER,
-                    DepartmentID INTEGER REFERENCES DEPARTMENT (DepartmentID)
+                    Department_ID INTEGER REFERENCES DEPARTMENT (Department_ID)
                     );
     CREATE TABLE OUTCOME (
-                    OutcomeID SERIAL PRIMARY KEY,
+                    Outcome_ID SERIAL PRIMARY KEY,
                     Name VARCHAR(30)
                     );
-    CREATE TABLE COURSEOUTCOME (
-                    CourseID INTEGER REFERENCES COURSE (CourseID),
-                    OutcomeID INTEGER REFERENCES OUTCOME (OutcomeID)
+    CREATE TABLE COURSE_OUTCOME (
+                    Course_ID INTEGER REFERENCES COURSE (Course_ID),
+                    Outcome_ID INTEGER REFERENCES OUTCOME (Outcome_ID)
                     );
     CREATE TABLE PREREQUISITE (
-                    CourseID INTEGER REFERENCES COURSE (CourseID),
-                    OutcomeID INTEGER REFERENCES OUTCOME (OutcomeID)
+                    Course_ID INTEGER REFERENCES COURSE (Course_ID),
+                    Outcome_ID INTEGER REFERENCES OUTCOME (Outcome_ID)
                     );
-    CREATE TABLE STUDENTGRADE (
-                    StudentID INTEGER REFERENCES STUDENT (StudentID),
-                    CourseID INTEGER REFERENCES COURSE (CourseID),
+    CREATE TABLE STUDENT_GRADE (
+                    Student_ID INTEGER REFERENCES STUDENT (Student_ID),
+                    Course_ID INTEGER REFERENCES COURSE (Course_ID),
                     Grade VARCHAR(2)
                     );
 
-    INSERT INTO REGUSER (Name, Surname, Email, Password, IsAdmin) 
-    VALUES ('admin','admin','admin@gmail.com', 'gAAAAABd9BaEELg95qbxr7i1H-bnoUGyjGnEBYjAnVOpXEZFvwCdUoDzPuIgny3W1ou9JwwiR-WeIv0YgPU21OKI7T2Tg5wgCA==', 1);
+    INSERT INTO REGUSER (Name, Surname, Email, Password, user_type) 
+    VALUES ('admin','admin','admin@gmail.com', 'gAAAAABd9BaEELg95qbxr7i1H-bnoUGyjGnEBYjAnVOpXEZFvwCdUoDzPuIgny3W1ou9JwwiR-WeIv0YgPU21OKI7T2Tg5wgCA==', 0);
 
-    INSERT INTO REGUSER (Name, Surname, Email, Password, IsAdmin) 
-    VALUES ('Iskender','Akyuz','akyuzi15@itu.edu.tr', 'gAAAAABd9BaEELg95qbxr7i1H-bnoUGyjGnEBYjAnVOpXEZFvwCdUoDzPuIgny3W1ou9JwwiR-WeIv0YgPU21OKI7T2Tg5wgCA==', 0);
+    INSERT INTO REGUSER (Name, Surname, Email, Password, user_type) 
+    VALUES ('Iskender','Akyuz','akyuzi15@itu.edu.tr', 'gAAAAABd9BaEELg95qbxr7i1H-bnoUGyjGnEBYjAnVOpXEZFvwCdUoDzPuIgny3W1ou9JwwiR-WeIv0YgPU21OKI7T2Tg5wgCA==', 2);
 
-    INSERT INTO STUDENT (StudentID, IdNumber) 
-    VALUES ((select UserID from REGUSER where REGUSER.Email='akyuzi15@itu.edu.tr'),'15016000');
+    INSERT INTO STUDENT (Student_ID, Id_Number) 
+    VALUES ((select User_ID from REGUSER where REGUSER.Email='akyuzi15@itu.edu.tr'),'15016000');
     """
 ]
 
