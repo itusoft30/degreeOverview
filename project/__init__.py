3347a2583422962d0fa2e9e783612db03dba782c
Memin <topuzm15@gmail.com>
2019-12-16 17:36:03 +0300

    Adding Forms
    We added forms and sign in operation.

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_login import LoginManager
+from project.config.crypto import Crypto
 
 app = Flask("project")
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from project.controllers import *
